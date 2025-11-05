de2d4a9e3fc3a9baae0752dafe6b03c7f59dabec
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-05 17:16:36 +0100

    added factory linking with the amount of seeders to be created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\Address;
+use App\Models\Certificate;
+use App\Models\Company;
+use App\Models\Email;
+use App\Models\EmailUser;
+use App\Models\Evaluation;
+use App\Models\FollowUpMaterial;
+use App\Models\FollowUpTest;
+use App\Models\Gdpr;
+use App\Models\PostalCode;
+use App\Models\Preparation;
+use App\Models\Requirement;
+use App\Models\Site;
+use App\Models\Training;
+use App\Models\TrainingUser;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +30,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        PostalCode::factory(30)->create();
+        Address::factory(50)->create();
+        Company::factory(10)->create();
+        User::factory(50)->create();
+        Site::factory(50)->create();
+        Gdpr::factory(50)->create();
+        Certificate::factory(10)->create();
+        Email::factory(20)->create();
+        Evaluation::factory(20)->create();
+        FollowUpTest::factory(4)->create();
+        Training::factory(100)->create();
+        FollowUpMaterial::factory(20)->create();
+        Preparation::factory(20)->create();
+        Requirement::factory(10)->create();
+        EmailUser::factory(100)->create();
+        TrainingUser::factory(100)->create();
     }
 }
