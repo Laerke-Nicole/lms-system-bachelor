1157af8fed77008cc3ab6707aec85910119de11e
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-05 17:39:19 +0100

    added workflow git for ci/cd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 
-    <!-- CSS and JS -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <!-- SCSS and JS -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
 </head>
 <body>
