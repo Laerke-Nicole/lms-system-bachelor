de71074902856bff8465bcd05a00e3cd695552b2
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-07 23:39:09 +0100

    finished log in view ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="vh-100 container-fluid">
+    <div class="vh-100 container-fluid login">
         <div class="row h-100">
             <!-- left side with form -->
-            <div class="col-12 col-lg-4 d-flex flex-column justify-content-center align-items-start">
-                <div class="px-5 d-flex flex-column w-100 bg-white">
+            <div class="col-12 col-lg-4 d-flex flex-column justify-content-center align-items-start login-left">
+                <div class="p-4 d-flex flex-column w-100 login-left__container">
                     <div class="mb-5">
                         <img src="{{ asset('images/default-logo.png') }}" alt="Logo" class="logo">
                     </div>
 
-                    <div class="w-100" style="max-width: 320px;">
+                    <div class="w-100">
                         <h3 class="mb-4">Log in</h3>
 
-                        <x-blocks.form action="{{ route('login') }}" method="POST">
+                        <x-blocks.form action="{{ route('login') }}" method="POST" class="mb-0">
                             <x-elements.input label="Email" placeholder="Enter your email" name="email" type="email" :value="old('email')" />
 
                             <x-elements.input label="Password" placeholder="Enter your password" name="password" type="password" />
 
                             <div class="d-flex flex-column mt-2">
                                 <a href="#" class="small mb-4 text-decoration-none text-secondary opacity-75 fs-5"><u>Forgot your password?</u></a>
-                                <button type="submit" class="btn btn-primary">Log in</button>
+                                <button type="submit" class="btn btn-primary mb-0">Log in</button>
                             </div>
 
                             <x-blocks.error-alert />
@@ -30,7 +30,7 @@
             </div>
 
             <!-- right side with img -->
-            <div class="col-lg-8 p-0">
+            <div class="col-lg-8 p-0 d-none d-lg-block">
 {{--                <img src="{{ asset('images/login-bg.jpg') }}"--}}
 {{--                     alt="Wind turbines"--}}
 {{--                     class="img-fluid w-100 h-100 object-fit-cover">--}}
