cfea782d540ebbcb9a50d68bea7c469ee7d45ce5
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-08 00:56:06 +0100

    added profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,16 @@
 
 <div id="app">
     <header>
-        <div class="p-3 d-lg-none">
-            <a href="#menu" class="btn fs-3">
+        <div class="p-3 d-flex align-items-center justify-content-between justify-content-lg-end">
+            <a href="#menu" class="btn fs-3 d-lg-none">
                 <span><i class="bi bi-list"></i></span>
             </a>
+
+            <ul class="list-unstyled d-flex mb-0">
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="#"><i class="bi bi-person-circle me-2 fs-4"></i><span>{{ Auth::user()->first_name }} {{ Auth::user()->last_name }} </span><i class="bi bi-three-dots-vertical"></i></a>
+                </li>
+            </ul>
         </div>
 
         <nav id="menu">
@@ -52,15 +58,6 @@
                 @endauth
             </ul>
         </nav>
-
-        {{--    <ul class="nav justify-content-end">--}}
-        {{--        <li class="nav-item">--}}
-        {{--            <a class="nav-link" href="#">Link</a>--}}
-        {{--        </li>--}}
-        {{--        <li class="nav-item">--}}
-        {{--            <a class="nav-link" href="#">Link</a>--}}
-        {{--        </li>--}}
-        {{--    </ul>--}}
     </header>
 
 
