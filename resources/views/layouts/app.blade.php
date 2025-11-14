9adb13c0040be42d6dacf1739c74be7adc5c9ee3
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-14 23:13:22 +0100

    added trainingslot index to app menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
                     </x-blocks.mmenu-dropdown>
 
                     <li><x-elements.link title="Trainings" href="{{ route('trainings.index') }}" icon="bi bi-mortarboard"></x-elements.link></li>
+                    <li><x-elements.link title="Training slots" href="{{ route('training_slots.index') }}" icon="bi bi-mortarboard"></x-elements.link></li>
 
                     <x-blocks.mmenu-dropdown title="Courses" icon="bi bi-journals">
                         <li><x-elements.link class="dropdown-item" title="Course" href="{{ route('courses.index') }}" icon="bi bi-dot"></x-elements.link></li>
