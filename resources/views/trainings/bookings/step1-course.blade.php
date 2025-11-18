f86091baf77f7915d7ec11f405b203664d5ab145
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-18 18:56:38 +0100

    created the steps in the bookings view

---
+++ resources/views/trainings/bookings/step1-course.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-md-6 col-lg-3">
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="" alt="" class="img-fluid">
+            </div>
+
+            <div class="col-md-6 col-lg-8">
+                <h4></h4>
+                <p></p>
+                <div class="d-flex justify-content-end">
+                    <i class="bi bi-arrow-right-short"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/trainings/bookings/step2-slot.blade.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-md-6 col-lg-3">
+        <div>
+            <div class="position-absolute top-0 left-0 m-1">
+                <p></p>
+            </div>
+            <img src="" alt="" class="img-fluid">
+        </div>
+
+        <div>
+            <h4></h4>
+            <p></p>
+
+            <div class="d-flex justify-content-around">
+                <div>
+                    <i class="bi bi-person"></i>
+                    <p></p>
+                </div>
+
+                <div>
+                    <i class="bi bi-arrow-right-short"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
