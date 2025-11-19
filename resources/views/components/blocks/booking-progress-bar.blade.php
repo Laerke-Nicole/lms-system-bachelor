ceb12b690ce8509fb37f72b81e006a68240ad651
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-19 16:37:59 +0100

    working on progress bar

--- resources/views/components/blocks/booking-progress-bar.blade.php
+++ resources/views/components/blocks/booking-progress-bar.blade.php
@@ -1,6 +1,42 @@
-<div class="progress-container">
-    <div class="step {{ $step >= 1 ? 'text-primary' : '' }}">1. Course</div>
-    <div class="step {{ $step >= 2 ? 'text-primary' : '' }}">2. Slot</div>
-    <div class="step {{ $step >= 3 ? 'text-primary' : '' }}">3. Employees</div>
-{{--    <div class="step {{ $step == 4 ? 'text-primary' : '' }}">4. Summary</div>--}}
+@props(['step'])
+
+<div class="booking-progress-bar position-fixed bottom-0 w-100 bg-white shadow-lg pt-3 pb-4 px-3">
+
+    {{-- Main progress line --}}
+    <div class="position-relative mb-3 px-3">
+        <div class="progress-line bg-light"></div>
+
+        <div class="progress-line-fill bg-dark"
+             style="width: calc(({{ $step }} - 1) / 3.80 * 100%);">
+        </div>
+    </div>
+
+    {{-- Steps --}}
+    <div class="d-flex justify-content-between text-center px-3">
+
+        <a href="{{ route('trainings.bookings.course') }}"
+           class="text-decoration-underline-hover small {{ $step >= 1 ? 'fw-bold' : 'text-muted' }}">
+            Course
+        </a>
+
+        <a href="{{ route('trainings.bookings.slot') }}"
+           class="text-decoration-underline-hover small {{ $step >= 2 ? 'fw-bold' : 'text-muted' }}">
+            Time
+        </a>
+
+        <a href="{{ route('trainings.bookings.employees') }}"
+           class="text-decoration-underline-hover small {{ $step >= 3 ? 'fw-bold' : 'text-muted' }}">
+            Employees
+        </a>
+
+        <a href="{{ route('trainings.bookings.summary') }}"
+           class="text-decoration-underline-hover small {{ $step >= 4 ? 'fw-bold' : 'text-muted' }}">
+            Confirm
+        </a>
+
+        <span class="text-decoration-underline-hover small {{ $step == 5 ? 'fw-bold' : 'text-muted' }}">
+            Done
+        </span>
+    </div>
+
 </div>
