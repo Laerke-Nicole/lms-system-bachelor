bd38918517847b43aceb976882d7649558b06f13
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-19 12:25:03 +0100

    styling 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,31 @@
+@extends('errors.layout')
 
+@section('message')
+
+    <div class="container-fluid">
+        <div class="row vh-100">
+            <!-- left side with message -->
+            <div class="col-12 col-lg-5 d-flex flex-column justify-content-center align-items-start">
+                <div class="p-4 d-flex flex-column w-100">
+                    <div class="w-100">
+                        <p class="fs-5 mb-2 text-primary">404</p>
+                        <h1 class="fs-60 mb-3">Page not found</h1>
+                        <p class="fs-4 mb-5">Sorry, we couldn't find the page.</p>
+                        <div>
+                            <a href="{{ route('home') }}" class="d-flex gap-1 fs-5 align-items-center text-primary">
+                                <i class="bi bi-arrow-left-short fs-3"></i>
+                                Go back to home
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- right side with img -->
+            <div class="col-12 col-lg-7 p-0 d-none d-lg-block">
+                <img src="https://cdn.pixabay.com/photo/2015/06/23/08/16/daegwallyeong-818420_1280.jpg" alt="" class="img-fluid w-100 h-100 object-fit-cover">
+            </div>
+        </div>
+    </div>
+
+@endsection
