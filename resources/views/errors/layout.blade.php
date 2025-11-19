97a0deafb1436538e8b7948ff1427ac6773e8270
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-19 12:23:37 +0100

    updated laravels error layout page to have the bootstrap and scss styling

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -6,48 +6,12 @@
 
         <title>@yield('title')</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 36px;
-                padding: 20px;
-            }
-        </style>
+        <!-- SCSS and JS -->
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title">
-                    @yield('message')
-                </div>
-            </div>
-        </div>
+    <body class="vh-100">
+        <main>
+            @yield('message')
+        </main>
     </body>
 </html>
