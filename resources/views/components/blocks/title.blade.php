e026a6fa4fd53ad6e29d1abc289f3f9485bd653f
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-19 18:31:12 +0100

    updated title component so it only adds btn if it has an href

--- resources/views/components/blocks/title.blade.php
+++ resources/views/components/blocks/title.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
-    <div class="col-lg-12 margin-tb">
+    <div class="col-lg-12 margin-tb mb-5">
         <div>
             <h3>{{ $title }}</h3>
             <p>{{ $tagline ?? null }}</p>
         </div>
 
-        @if(! $attributes->has('withoutButton'))
-            <div class="mb-5">
+        @if($attributes->has('href'))
+            <div>
                 <a class="btn btn-primary" href="{{ $attributes->get('href') }}">{{ $buttonText ?? 'Submit' }}</a>
             </div>
         @endif
