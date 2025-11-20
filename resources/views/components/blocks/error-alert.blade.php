6c2393b94c4271bba1e249ac8bb9142ced4a1ba7
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-20 22:29:24 +0100

    updated ui of alert msg with an icon and better title

--- resources/views/components/blocks/error-alert.blade.php
+++ resources/views/components/blocks/error-alert.blade.php
@@ -1,9 +1,12 @@
 @if ($errors->any())
     <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
+        <strong>Whoops!</strong> There were some problems.<br><br>
+        <ul class="list-unstyled">
             @foreach ($errors->all() as $error)
-                <li class="mb-3">{{ $error }}</li>
+                <li class="mb-3">
+                    <i class="bi bi-exclamation-triangle me-2"></i>
+                    {{ $error }}
+                </li>
             @endforeach
         </ul>
     </div>
