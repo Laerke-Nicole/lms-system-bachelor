01ba068285afce2c902c5de6a93450434ce9c54e
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-20 22:27:45 +0100

    made frontend for step 3

--- resources/views/trainings/bookings/step3-employees.blade.php
+++ resources/views/trainings/bookings/step3-employees.blade.php
@@ -2,47 +2,86 @@
 
 @section('content')
 
-    <h3 class="mb-5">Who's joining?</h3>
+{{--    {{ Breadcrumbs::render('bookings.employees') }}--}}
 
-    <div class="row g-3">
-        @foreach($employees as $employee)
-            <div class="col-12 col-md-6 col-lg-3">
-                <x-blocks.form action="{{ route('trainings.bookings.slot.store') }}" method="POST" class="mb-0 w-100 d-flex flex-column">
-                    <input type="hidden" name="user_ids" value="{{ $employee->id }}">
+<div class="row booking-section">
+    <div class="col-12 col-lg-7 booking-section-small">
+        <div class="row">
+            <h3 class="mb-4">Who is attending?</h3>
 
-                    <button type="submit" class="w-100 border-0 bg-white rounded-3 shadow-sm text-start d-flex flex-column p-0">
+            <x-blocks.error-alert />
 
-                        <div class="position-relative">
-                            <div class="position-absolute top-0 left-0 m-2 z-1">
-                                <p class="p-1 bg-light text-label rounded-1">{{ $trainingSlot->place }}</p>
-                            </div>
-                            <div class="step-cards__card__image-wrapper2 img-hover-scale">
-                                <img src="{{ asset('storage/' . $trainingSlot->course->image) ?? '/placeholder.png' }}"
-                                     alt="{{ $trainingSlot->course->title }}"
-                                     class="step-cards__card__image-wrapper2__image object-fit-cover position-relative h-34 w-100">
-                            </div>
-                        </div>
-
-                        <div class="p-3">
-                            <p class="text-primary text-label mb-1">{{ $trainingSlot->course->title }}</p>
-                            <h4 class="mb-3">{{ $trainingSlot->training_date->format('d M Y, H:i') }}</h4>
-
-                            <div class="d-flex justify-content-between">
-                                <div class="d-flex justify-content-center gap-2">
-                                    <i class="bi bi-person text-muted"></i>
-                                    <p class="fs-5 mb-0 text-muted">{{ $trainingSlot->trainer->first_name }} {{ $trainingSlot->trainer->last_name }}</p>
+            <x-blocks.form action="{{ route('trainings.bookings.employees.store') }}" method="POST">
+                @foreach($employees as $employee)
+                    <div class="row g-3">
+                        <div class="col-12 col-lg-4">
+                            <label class="d-flex py-3">
+                                <div class="d-flex gap-3">
+                                    {{-- checkbox --}}
+                                    <input type="checkbox"
+                                           name="user_ids[]"
+                                           value="{{ $employee->id }}"
+                                           class="form-check-input">
+                                    {{-- name and email of the employee --}}
+                                    <div>
+                                        <h5 class="mb-1 fw-semibold">{{ $employee->first_name }} {{ $employee->last_name }}</h5>
+                                        <p class="text-muted small mb-0">{{ $employee->email }}</p>
+                                    </div>
                                 </div>
-
-                                <div>
-                                    <i class="bi bi-chevron-right text-primary"></i>
-                                </div>
-                            </div>
+                            </label>
                         </div>
-                    </button>
-                </x-blocks.form>
-            </div>
-        @endforeach
+                    </div>
+                @endforeach
+
+
+                <button type="submit" class="btn btn-primary mt-3">Continue</button>
+
+            </x-blocks.form>
+        </div>
     </div>
+
+    <div class="col-12 col-lg-5">
+        <div class="small p-4 d-flex flex-column gap-2 bg-light rounded shadow-sm">
+            <h3>Booking overview</h3>
+            <div class="d-flex justify-content-between">
+                <p>Course</p>
+                <p>{{ $course->title }}</p>
+            </div>
+
+            <div class="d-flex justify-content-between ">
+                <p>Duration</p>
+                <p>8 hrs</p>
+                {{--            <p>{{ $course->duration }}</p>--}}
+            </div>
+
+            <div class="d-flex justify-content-between">
+                <p class="mb-0">{{ $course->description }}</p>
+            </div>
+
+            <hr>
+
+            <div class="d-flex justify-content-between">
+                <p>Date</p>
+                <p>{{ $trainingSlot->training_date->format('d M Y, H:i') }}</p>
+            </div>
+
+            <div class="d-flex justify-content-between">
+                <p>Trainer</p>
+                <p>{{ $trainingSlot->trainer->first_name }} {{ $trainingSlot->trainer->last_name }}</p>
+            </div>
+
+            <div class="d-flex justify-content-between">
+                <p>Location</p>
+                <p>{{ $trainingSlot->place }}</p>
+            </div>
+
+            <div class="d-flex">
+                <button type="submit" class="btn btn-primary">Continue</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
 
