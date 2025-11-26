be48df3ce352233dc91c2429924e4402411817a1
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-26 20:04:31 +0100

    fixed small errors to make the crud for trainings work properly

--- resources/views/trainings/edit.blade.php
+++ resources/views/trainings/edit.blade.php
@@ -18,27 +18,15 @@
     <x-blocks.form action="{{ route('trainings.update', $training->id) }}" method="POST">
         @method('PUT')
 
-{{--        <x-elements.input label="Date" name="training_date" type="datetime-local" value="{{ $training->training_date }}" />--}}
+        <x-elements.input label="Date" name="training_date" type="datetime-local" value="{{ $training->trainingSlot->training_date }}" />
 
-{{--        <x-elements.select label="Course" name="course">--}}
-{{--            @foreach($courses as $course)--}}
-{{--                <option value="{{ $course->id }}" {{ $training->course === $course ? 'selected' : '' }}>{{ $course->title }}</option>--}}
-{{--            @endforeach--}}
-{{--        </x-elements.select>--}}
+        <x-elements.select label="Place" name="place">
+            @foreach($places as $place)
+                <option value="{{ $place }}" {{ $training->trainingSlot->place === $place ? 'selected' : '' }}>{{ $place }}</option>
+            @endforeach
+        </x-elements.select>
 
-{{--        <x-elements.select label="Place" name="place">--}}
-{{--            @foreach($places as $place)--}}
-{{--                <option value="{{ $place }}" {{ $training->place === $place ? 'selected' : '' }}>{{ $place }}</option>--}}
-{{--            @endforeach--}}
-{{--        </x-elements.select>--}}
-
-{{--        <x-elements.select label="Trainer" name="trainer">--}}
-{{--            @foreach($trainers as $trainer)--}}
-{{--                <option value="{{ $trainer->id }}">{{ $trainer->first_name }} {{ $trainer->last_name }}</option>--}}
-{{--            @endforeach--}}
-{{--        </x-elements.select>--}}
-
-{{--        <x-elements.input label="Whatsapp link (optional, can be added later)" placeholder="Whatsapp link" name="participation_link" type="url" value="{{ $training->participation_link }}" />--}}
+        <x-elements.input label="Whatsapp link (optional, can be added later)" placeholder="Whatsapp link" name="participation_link" type="url" value="{{ $training->trainingSlot->participation_link }}" />
 
         <div class="d-flex flex-wrap align-items-baseline gap-2">
             <button type="submit" class="btn btn-primary">Submit</button>
--- resources/views/trainings/show.blade.php
+++ resources/views/trainings/show.blade.php
@@ -11,17 +11,17 @@
         $training
     ) }}
 
-    <x-blocks.title title="Training for: {{ $training->course->title }}"></x-blocks.title>
+    <x-blocks.title title="Training for {{ $training->trainingSlot->course->title }}"></x-blocks.title>
 
     <div class="row">
-        <x-blocks.detail field="Date" title="{{ $training->training_slot->training_date->format('d M Y H:i') }}"></x-blocks.detail>
-        <x-blocks.detail field="Course" title="{{ $training->training_slot->course->title }}"></x-blocks.detail>
-        <x-blocks.detail field="Place" title="{{ $training->training_slot->place }}"></x-blocks.detail>
-        <x-blocks.detail field="Trainer" title="{{ $training->training_slot->trainer->first_name. ' ' . ($training->trainer)->last_name }}"></x-blocks.detail>
+        <x-blocks.detail field="Date" title="{{ $training->trainingSlot->training_date->format('d M Y H:i') }}"></x-blocks.detail>
+        <x-blocks.detail field="Course" title="{{ $training->trainingSlot->course->title }}"></x-blocks.detail>
+        <x-blocks.detail field="Place" title="{{ $training->trainingSlot->place }}"></x-blocks.detail>
+        <x-blocks.detail field="Trainer" title="{{ $training->trainingSlot->trainer->first_name}} {{$training->trainingSlot->trainer->last_name }}"></x-blocks.detail>
         <x-blocks.detail field="Ordered by" title="{{ $training->orderedBy->first_name. ' ' . ($training->orderedBy)->last_name }}"></x-blocks.detail>
-        <x-blocks.detail field="Whatsapp link" title="{{ $training->participation_link }}"></x-blocks.detail>
+        <x-blocks.detail field="Whatsapp link" title="{{ $training->trainingSlot->participation_link }}"></x-blocks.detail>
         @if($training->status === 'Upcoming')
-            <x-blocks.detail field="Reminder before training" title="{{ $training->reminder_before_training_formatted }}"></x-blocks.detail>
+            <x-blocks.detail field="Reminder before training" title="{{ $training->reminder_before_training ?? 'No' }} "></x-blocks.detail>
         @endif
         @if(in_array($training->status, ['Completed', 'Expired']))
             <x-blocks.detail field="Reminder sent 18 months" title="{{ $training->reminder_sent_18_m ? 'Yes' : 'No' }}"></x-blocks.detail>
