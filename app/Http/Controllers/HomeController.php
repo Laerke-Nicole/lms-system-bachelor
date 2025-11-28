8352dec809ab9cbcf5e4268fc775fdb7989418db
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-29 00:48:26 +0100

    added on home page participate in training when the user has an upcoming training

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,17 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
-        return view('home');
+    public function index()
+    {
+//        get the trainings that the user has today or starting within 24 hours
+        $participateTraining = auth()->user()->trainings()
+            ->where('status', 'Upcoming')
+            ->whereHas('trainingSlot', function($query) {
+                $query->whereBetween('training_date', [now(), now()->addDay()]);
+            })
+            ->with('trainingSlot')
+            ->get();
+
+        return view('home', compact('participateTraining'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,19 @@
 @extends('layouts.app')
+
+@section('content')
+
+    <div class="row">
+        @forelse($participateTraining as $training)
+            <div class="col-12">
+                <h3>You have an upcoming training</h3>
+                <p>Participate in our Whatsapp call and start your training on {{ $training->trainingSlot->training_date->format('d M Y') }} at {{ $training->trainingSlot->training_date->format('H:i') ?? null }} UTC.</p>
+                <a href="{{ $training->trainingSlot->participation_link }}" target="_blank" class="btn btn-secondary">Participate</a>
+            </div>
+        @empty
+            <div class="col-12">
+                <p>You have no trainings starting soon.</p>
+            </div>
+        @endforelse
+    </div>
+
+@endsection
