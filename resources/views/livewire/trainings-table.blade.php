c3ee6bf8160a7e0e0763552de736869a1df2b9d5
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-11-29 01:36:24 +0100

    updated new icon button for participate in training

--- resources/views/livewire/trainings-table.blade.php
+++ resources/views/livewire/trainings-table.blade.php
@@ -38,7 +38,7 @@
                             <li><hr class="dropdown-divider"></li>
                             <li>
                                 <a class="dropdown-item fs-5" href="{{ $training->trainingSlot->participation_link }}" target="_blank">
-                                    <i class="bi bi-clipboard-check me-2"></i>Participate in training
+                                    <i class="bi bi-person-workspace me-2"></i>Participate in training
                                 </a>
                             </li>
                         @endif
