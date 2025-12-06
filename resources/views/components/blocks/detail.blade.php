44c0a67764f3d0ef7611ce2cdcdd629d25ac98ff
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-06 14:40:24 +0100

    updated the show page to have a prettier aligned format

--- resources/views/components/blocks/detail.blade.php
+++ resources/views/components/blocks/detail.blade.php
@@ -1,19 +1,34 @@
 @props(['isImage' => false, 'class' => null, 'title' => null, 'field' => null, 'secondTitle' => null, 'thirdTitle' => null, 'valueClass' => null])
 
 <div class="{{ $col ?? 'col-12'}}">
-    <div class="form-group d-flex">
+    <div class="form-group">
 
         {{ $slot }}
 
-        <div>
-            <p class="text-dark mb-1 fs-4 {{ $class }}">{{ $field }}</p>
-            @if($isImage)
-                @if($title)
-                    <img src="{{ asset('storage/' . $title) }}" alt="{{ basename($title) }}" class="{{ $imageClass ?? 'w-60 img-fluid mb-4' }}">
-                @endif
-            @else
-                <p class="{{ $valueClass ?? 'lh-md mb-4' }}">{{ $title }} @if($secondTitle)<br>{{ $secondTitle }}@endif @if($thirdTitle)<br>{{ $thirdTitle }}@endif</p>
-            @endif
-        </div>
+        <dl class="row mb-3">
+{{--            label --}}
+            <dt class="col-sm-2 text-dark {{ $class }}">{{ $field }}</dt>
+
+{{--            the value --}}
+            <dd class="col-sm-10 {{ $valueClass ?? 'lh-md' }}">
+                @if($isImage)
+                    @if($title)
+                        <img src="{{ asset('storage/' . $title) }}"
+                             alt="{{ basename($title) }}"
+                             class="{{ $imageClass ?? 'w-60 img-fluid' }}">
+                    @endif
+                @else
+                        {{ $title }}
+
+                        @if($secondTitle)
+                            <br>{{ $secondTitle }}
+                        @endif
+
+                        @if($thirdTitle)
+                            <br>{{ $thirdTitle }}
+                        @endif
+               @endif
+            </dd>
+        </dl>
     </div>
 </div>
