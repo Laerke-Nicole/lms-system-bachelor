c13ca9d187bcc036e0097774545866a88bb530a8
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-06 23:38:11 +0100

    making input component more reusable

--- resources/views/components/elements/input.blade.php
+++ resources/views/components/elements/input.blade.php
@@ -1,6 +1,6 @@
 <div class="{{ $col ?? 'col-12 col-lg-4' }}">
-    <div class="form-group mb-3">
-        <label class="form-label text-label-1" for="{{ $labelFor ?? null }}">{{ $label }}</label>
+    <div class="form-group mb-3 {{ $formGroupClass ?? null }}">
+        <label class="form-label {{ $labelClass ?? 'text-label-1'}}" for="{{ $labelFor ?? null }}">{{ $label }}</label>
         <input
             type="{{ $type ?? 'text' }}"
             name="{{ $name }}"
