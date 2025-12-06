288d25b028f8dff5d1315c6597c39acf096f489e
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-06 23:46:04 +0100

    show signature page

--- resources/views/livewire/trainings-table.blade.php
+++ resources/views/livewire/trainings-table.blade.php
@@ -64,6 +64,16 @@
                                     </a>
                                 </li>
                             @endif
+
+{{--                            show signature page if the user completed test and evalution and didnt sign yet --}}
+{{--                                @if($training->trainingUsers->completed_test_at && $training->trainingUsers->completed_evaluation_at && !$training->trainingUsers->signed_at)--}}
+                                    <li>
+{{--                                        pass the training id to the signature page --}}
+                                        <a class="dropdown-item fs-5" href="{{ route('signatures.index', ['training_id' => $training->id]) }}">
+                                            <i class="bi bi-pencil me-2"></i>Sign to get certificate
+                                        </a>
+                                    </li>
+{{--                                @endif--}}
                         @endif
                     </x-blocks.table-actions>
                 </td>
