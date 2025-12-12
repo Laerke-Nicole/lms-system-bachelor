b5feb14afe3135ab40dec3da0f948413e0ac26e5
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-12 17:45:58 +0100

    delete old route i dont see anymore

--- routes/certificate.php
+++ routes/certificate.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\CertificateController;
 
 Route::middleware(['auth', 'role:user,leader'])->group(function () {
     Route::get('/certificates/{trainingUser}/preview', [CertificateController::class, 'preview'])->name('certificates.certificate-preview');
-    Route::get('/certificates', [CertificateController::class, 'index'])->name('certificates.index');
-    Route::get('/certificates/{training_id}/view', [CertificateController::class, 'viewCertificate'])->name('certificates.viewCertificate');
+    Route::get('/certificates/{training_id}/confirmation', [CertificateController::class, 'certificateConfirmation'])->name('certificates.certificate-confirmation');
     Route::get('/certificates/{training_id}', [CertificateController::class, 'certificatePdf'])->name('certificates.certificatePdf');
 });
