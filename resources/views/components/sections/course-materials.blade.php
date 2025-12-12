c42f4e962b10deea7233311cb7f89afc52e0ce08
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-12 19:24:27 +0100

    only display the course materials that have a pdf or url

--- resources/views/components/sections/course-materials.blade.php
+++ resources/views/components/sections/course-materials.blade.php
@@ -13,42 +13,46 @@
         <div class="row g-4 px-xl-5 course-materials">
     @if($training->trainingSlot && $training->trainingSlot->course)
         @forelse ($training->trainingSlot->course->courseMaterials as $material)
-            <div class="col-lg-4">
-                <div class="d-flex flex-row">
-                    <div>
-{{--                        icon --}}
-                        @if($material->type)
-                            <div class="pe-none bg-primary text-light p-2 me-4 rounded-circle">
-                                @if($material->type === 'Video')
-                                    <i class="bi bi-camera-reels course-materials__icon"></i>
-                                @elseif ($material->type === 'PDF')
-                                    <i class="bi bi-file-earmark-pdf course-materials__icon"></i>
-                                @elseif ($material->type === 'Task')
-                                    <i class="bi bi-pencil course-materials__icon"></i>
-                                @elseif ($material->type === 'Quiz')
-                                    <i class="bi bi-patch-question course-materials__icon"></i>
-                                @else
-                                    <i class="bi bi-paperclip course-materials__icon"></i>
-                                @endif
-                            </div>
-                        @endif
-                    </div>
-                    <div>
-                        <h4>{{ $material->type }}</h4>
-                        <p class="mb-2">{{ $material->title }}</p>
-                        @if($material->type === 'PDF' && $material->pdf)
-                            <a href="{{ asset('storage/' . $material->pdf) }}" target="_blank" class="text-primary">
-                                Download PDF
-                            </a>
-                        @elseif($material->url)
-                            <a href="{{ $material->url }}" target="_blank" class="text-primary">
-                                Open Material
-                                <i class="bi bi-arrow-up-right ms-1"></i>
-                            </a>
-                        @endif
+{{--            if there is not pdf or url attached then dont show the material --}}
+            @if($material->pdf || $material->url)
+                <div class="col-lg-4">
+                    <div class="d-flex flex-row">
+                        <div>
+    {{--                        icon --}}
+                            @if($material->type)
+                                <div class="pe-none bg-primary text-light p-2 me-4 rounded-circle">
+                                    @if($material->type === 'Video')
+                                        <i class="bi bi-camera-reels course-materials__icon"></i>
+                                    @elseif ($material->type === 'PDF')
+                                        <i class="bi bi-file-earmark-pdf course-materials__icon"></i>
+                                    @elseif ($material->type === 'Task')
+                                        <i class="bi bi-pencil course-materials__icon"></i>
+                                    @elseif ($material->type === 'Quiz')
+                                        <i class="bi bi-patch-question course-materials__icon"></i>
+                                    @else
+                                        <i class="bi bi-paperclip course-materials__icon"></i>
+                                    @endif
+                                </div>
+                            @endif
+                        </div>
+                        <div>
+                            <h4>{{ $material->type }}</h4>
+                            <p class="mb-2">{{ $material->title }}</p>
+                            @if($material->type === 'PDF' && $material->pdf)
+                                <a href="{{ asset('storage/' . $material->pdf) }}" target="_blank" class="text-primary">
+                                    Download PDF
+                                    <i class="bi bi-download ms-1"></i>
+                                </a>
+                            @elseif($material->url)
+                                <a href="{{ $material->url }}" target="_blank" class="text-primary">
+                                    Open Material
+                                    <i class="bi bi-arrow-up-right ms-1"></i>
+                                </a>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         @empty
             <p class="text-muted">No course materials available.</p>
         @endforelse
