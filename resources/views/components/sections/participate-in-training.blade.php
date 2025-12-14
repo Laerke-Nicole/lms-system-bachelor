25aa336fbf3d24cf4f3bfeb985aeb962d616c59b
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-14 12:49:18 +0100

    redid the particiapte in trianing ui

--- resources/views/components/sections/participate-in-training.blade.php
+++ resources/views/components/sections/participate-in-training.blade.php
@@ -1,25 +1,30 @@
 @props(['participateTraining'])
 
-@if($participateTraining->isNotEmpty())
+@if( auth()->user()->role === 'user' || auth()->user()->role === 'leader' )
+    @if($participateTraining->isNotEmpty())
+        <div class="px-12">
+            <div class="row">
+                @foreach($participateTraining as $training)
+                    @if($training->trainingSlot && $training->trainingSlot->course && $training->trainingSlot->training_date)
 
-    <div class="row bg-primary section-box">
+                        <div class="alert alert-success" role="alert">
+                            <h4>
+                                <button class="btn btn-success py-2 me-2 pe-none">Reminder</button>
+                                You have an upcoming training</h4>
+                            <p>
+                                Participate in your upcoming {{ $training->trainingSlot->course->title }} training on
+                                {{ $training->trainingSlot->training_date->format('d M Y') }} at
+                                {{ $training->trainingSlot->training_date->format('H:i') }} UTC —
+                                <a href="{{ $training->trainingSlot->participation_link }}" target="_blank" class="text-decoration-underline">
+                                    participate now <i class="bi bi-arrow-right ms-1"></i>
+                                </a>
+                            </p>
 
-        @forelse($participateTraining as $training)
-            @if($training->trainingSlot && $training->trainingSlot->course && $training->trainingSlot->training_date)
-                <div class="col-lg-10 col-xl-9 col-xxl-8 mx-auto text-center">
-                    <p class="text-white">You have an upcoming training</p>
-                    <h3 class="text-white mb-4 display-1 fw-bold letter-spacing-normal">
-                        Participate in your upcoming {{ $training->trainingSlot->course->title }} training
-                        on {{ $training->trainingSlot->training_date->format('d M Y') }}
-                        at {{ $training->trainingSlot->training_date->format('H:i') }} UTC.
-                    </h3>
-                    @if($training->trainingSlot->participation_link)
-                        <a href="{{ $training->trainingSlot->participation_link }}" target="_blank" class="btn btn-light">Participate now</a>
+                        </div>
+
                     @endif
-                </div>
-            @endif
-        @empty
-
-        @endforelse
-    </div>
+                @endforeach
+            </div>
+        </div>
+    @endif
 @endif
