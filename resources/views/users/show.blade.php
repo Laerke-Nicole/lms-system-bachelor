d317edf4edbe502dd05c5397e22742ad36d20864
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-14 16:02:40 +0100

    if statements to show different content on user based on user role

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,20 +2,31 @@
 
 @section('content')
 
-    {{ Breadcrumbs::render(
+    @if(auth()->user()->role === 'admin')
+        {{ Breadcrumbs::render(
         'crud.show',
-        'Users',
+        'Participants',
         'users.index',
         'View',
         'users.show',
         $user
     ) }}
+    @elseif(auth()->user()->role === 'leader')
+        {{ Breadcrumbs::render(
+        'crud.show',
+        'Employees',
+        'users.index',
+        'View',
+        'users.show',
+        $user
+    ) }}
+    @endif
 
-    <x-blocks.title title="{{ $gdpr->title }}" />
+    <x-blocks.title title="{{ $user->first_name }} {{ $user->last_name }}" />
 
     <div class="row">
-        <x-blocks.detail field="Title" title="{{ $gdpr->title }}" />
-        <x-blocks.detail field="Content" title="{{ $gdpr->content }}" />
+        <x-blocks.detail field="Email" title="{{ $user->email }}" />
+        <x-blocks.detail field="Phone" title="{{ $user->phone }}" />
     </div>
 
 @endsection
