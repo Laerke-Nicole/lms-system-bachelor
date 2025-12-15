c886446c65d760ff01c74f211509084a1f9b8957
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-15 21:15:01 +0100

    made error page 419

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,31 @@
+@extends('errors.layout')
+
+@section('message')
+
+    <div class="container-fluid">
+        <div class="row vh-100">
+            <!-- left side with message -->
+            <div class="col-12 col-lg-5 d-flex flex-column justify-content-center align-items-start">
+                <div class="d-flex flex-column w-100">
+                    <div class="w-100">
+                        <p class="fs-5 mb-2 text-primary">419</p>
+                        <h1 class="fs-60 mb-3">Page expired</h1>
+                        <p class="fs-4 mb-5">Sorry, the page has expired.</p>
+                        <div>
+                            <a href="{{ route('home') }}" class="text-decoration-underline-hover d-flex gap-1 fs-5 align-items-center text-primary">
+                                <i class="bi bi-arrow-left-short fs-3"></i>
+                                Go back to home
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- right side with img -->
+            <div class="col-12 col-lg-7 p-0 d-none d-lg-block">
+                <img src="storage/standard-windmill-image.jpg" alt="windmill" class="windmill-image img-fluid w-100 h-100 object-fit-cover">
+            </div>
+        </div>
+    </div>
+
+@endsection
