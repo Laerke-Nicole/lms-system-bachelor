872bea168ba9ac17df0c483ea5b3b2356c84117e
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-17 23:39:11 +0100

    added company and site name to user index and show page if the user role is admin

--- resources/views/crud/users/index.blade.php
+++ resources/views/crud/users/index.blade.php
@@ -12,28 +12,49 @@
 
     <x-blocks.message/>
 
-    <x-blocks.table-head :headers="['Full name', 'Email', 'Phone', 'Actions']">
-        @forelse ($users as $user)
-            <tr>
-                <td>{{ $user->first_name }} {{ $user->last_name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->phone }}</td>
-                <td>
-                    <x-blocks.table-actions :showRoute="route('users.show', $user->id)"
-                                            :deleteRoute="route('users.destroy', $user->id)"/>
-                </td>
-            </tr>
-        @empty
-            <tr>
-                @if(auth()->user()->role === 'admin')
-                    <td colspan="3">There are no participants.</td>
-                @elseif(auth()->user()->role === 'leader')
-                    <td colspan="3">There are no employees.</td>
-                @endif
-            </tr>
-        @endforelse
-    </x-blocks.table-head>
-
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col" class="text-label-1 text-body">Full name</th>
+            <th scope="col" class="text-label-1 text-body">Email</th>
+            <th scope="col" class="text-label-1 text-body">Phone</th>
+            @if(auth()->user()->role === 'admin')
+                <th scope="col" class="text-label-1 text-body">Company</th>
+            @endif
+            <th scope="col" class="text-label-1 text-body">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+            @forelse ($users as $user)
+                <tr>
+                    <td>{{ $user->first_name }} {{ $user->last_name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->phone }}</td>
+                    @if(auth()->user()->role === 'admin')
+                        @if($user->site->company->company_name)
+                            <td>{{ $user->site->company->company_name }}
+                                @if($user->site->site_name)
+                                    <br><span class="text-muted">{{ $user->site->site_name }}</span>
+                                @endif
+                            </td>
+                        @endif
+                    @endif
+                    <td>
+                        <x-blocks.table-actions :showRoute="route('users.show', $user->id)"
+                                                :deleteRoute="route('users.destroy', $user->id)"/>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    @if(auth()->user()->role === 'admin')
+                        <td colspan="3">There are no participants.</td>
+                    @elseif(auth()->user()->role === 'leader')
+                        <td colspan="3">There are no employees.</td>
+                    @endif
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 
     <x-elements.pagination :items="$users"/>
 
--- resources/views/crud/users/show.blade.php
+++ resources/views/crud/users/show.blade.php
@@ -25,8 +25,20 @@
     <x-blocks.title title="{{ $user->first_name }} {{ $user->last_name }}" />
 
     <div class="row">
-        <x-blocks.detail field="Email" title="{{ $user->email }}" />
-        <x-blocks.detail field="Phone" title="{{ $user->phone }}" />
+        @if($user->email)
+            <x-blocks.detail field="Email" title="{{ $user->email }}" />
+        @endif
+        @if($user->phone)
+            <x-blocks.detail field="Phone" title="{{ $user->phone }}" />
+        @endif
+        @if(auth()->user()->role === 'admin')
+            @if($user->site->company->company_name)
+                <x-blocks.detail field="Company" title="{{ $user->site->company->company_name }}" />
+            @endif
+            @if($user->site->site_name)
+                <x-blocks.detail field="Site" title="{{ $user->site->site_name }}" />
+            @endif
+        @endif
     </div>
 
 @endsection
