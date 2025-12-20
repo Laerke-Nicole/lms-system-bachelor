c1c8e22a45335e9e46e5ca1d743b9478a090d663
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-20 23:06:11 +0100

    laravel dot for includes

--- resources/views/livewire/trainings-table.blade.php
+++ resources/views/livewire/trainings-table.blade.php
@@ -1,10 +1,10 @@
 <div>
 {{--    filtering btns with active and inactive state --}}
-    @include('components/elements/trainings-filter')
+    @include('components.elements.trainings-filter')
 
     <x-blocks.trainings-table-head :filter="$filter">
 {{--        body of the table --}}
-        @include('components/blocks/trainings-table-body')
+        @include('components.blocks.trainings-table-body')
     </x-blocks.trainings-table-head>
 
     <x-elements.pagination :items="$this->trainings"/>
