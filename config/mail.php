a48ce648375a00db0e2dba0848a8f9d2275dba02
Lærke Nielsen <nicolenielsen85@gmail.com>
2025-12-25 19:11:09 +0100

    mail timeout

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => 10,
+            'timeout' => env('MAIL_TIMEOUT', 10),
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
