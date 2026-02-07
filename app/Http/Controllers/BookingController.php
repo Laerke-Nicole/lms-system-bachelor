416ac2f14ce3af7329d312b0091ae19dcd467ed5
Lærke Nielsen <nicolenielsen85@gmail.com>
2026-02-07 17:31:47 +0100

    testing

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -125,11 +125,7 @@ class BookingController extends Controller
                 'trainer_id' => null,
             ]);
         } catch (QueryException $e) {
-            if ($e->getCode() === '23000') {
-                return back()->withErrors(['training_day' => 'That day is already taken. Please pick another.']);
-            }
-
-            throw $e;
+            return back()->withErrors(['training_day' => 'DB Error: ' . $e->getMessage()]);
         }
 
         session(['booking.training_slot_id' => $slot->id]);
